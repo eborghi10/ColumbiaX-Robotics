c74315ee9dc74627c8c25a5f4d640ff2753f5951
eborghi10 <emiliano_borghi@hotmail.com>
2017-04-25 14:13:51 -0300

    It's time to start coding

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 
 3) **project2_solution**: program that uses tf to describe the poses between a robot, its camera and an object.
 
-4) **marker_publisher**: used in _project2_solution_ to visualize the transformations. It's invoked by the other package.
+4) **marker_publisher**: used in _project2_solution_ to visualize the transformations. It's invoked by _project2_solution_.
+
+5) **project3**: packages that analyze the forward dynamics of a Kuka LBR iiwa arm.
--- project3/forward_kinematics/src/solution.py
+++ project3/forward_kinematics/src/solution.py
@@ -9,9 +9,11 @@ import tf
 import tf.msg
 from urdf_parser_py.urdf import URDF
 
-""" Starting from a computed transform T, creates a message that can be
+""" 
+Starting from a computed transform T, creates a message that can be
 communicated over the ROS wire. In addition to the transform itself, the message
-also specifies who is related by this transform, the parent and the child."""
+also specifies who is related by this transform, the parent and the child.
+"""
 def convert_to_message(T, child, parent):
     t = geometry_msgs.msg.TransformStamped()
     t.header.frame_id = parent
@@ -28,25 +30,28 @@ def convert_to_message(T, child, parent):
     t.transform.rotation.w = rotation[3]        
     return t
     
-#Our main class for computing Forward Kinematics
+# Our main class for computing Forward Kinematics
 class ForwardKinematics(object):
 
 
-    #Initialization
+    # Initialization
     def __init__(self):
-        """Announces that it will publish forward kinematics results, in the form of tfMessages.
+        """
+        Announces that it will publish forward kinematics results, in the form of tfMessages.
         "tf" stands for "transform library", it's ROS's way of communicating around information
-        about where things are in the world"""
+        about where things are in the world
+        """
         self.pub_tf = rospy.Publisher("/tf", tf.msg.tfMessage, queue_size=1)
 
-        #Loads the robot model, which contains the robot's kinematics information
+        # Loads the robot model, which contains the robot's kinematics information
         self.robot = URDF.from_parameter_server()
 
-        #Subscribes to information about what the current joint values are.
+        # Subscribes to information about what the current joint values are.
         rospy.Subscriber("joint_states", JointState, self.callback)
 
 
-    """This function is called every time the robot publishes its joint values. We must use
+    """
+    This function is called every time the robot publishes its joint values. We must use
     the information we get to compute forward kinematics.
 
     We will iterate through the entire chain, and publish the transform for each link we find.
@@ -85,19 +90,20 @@ class ForwardKinematics(object):
         self.pub_tf.publish(all_transforms)
 
 
-    """ This is the function that performs the main forward kinematics computation. It accepts as 
-    parameters all the information needed about the joints and links of the robot, as well as the current
-    values of all the joints, and must compute and return the transforms from the world frame to all the 
-    links, ready to be published through tf.
+    """ 
+    This is the function that performs the main forward kinematics computation. It accepts as 
+    parameters all the information needed about the joints and links of the robot, as well as
+    the current values of all the joints, and must compute and return the transforms from the
+    world frame to all the links, ready to be published through tf.
 
     Parameters are as follows:
     - link_names: a list with all the names of the robot's links, ordered from proximal to distal.
-    These are also the names of the link's respective coordinate frame. In other words, the transform
-    from the world to link i should be published with "world_link" as the parent frame and link_names[i] 
-    as the child frame.    
+    These are also the names of the link's respective coordinate frame. In other words, the
+    transform from the world to link i should be published with "world_link" as the parent frame
+    and link_names[i] as the child frame.    
 
-    - joints: a list of all the joints of the robot, in the same order as the links listed above. Each 
-    entry in this list is an object which contains the following fields:
+    - joints: a list of all the joints of the robot, in the same order as the links listed above.
+    Each entry in this list is an object which contains the following fields:
      * joint.origin.xyz: the translation from the frame of the previous joint to this one
      * joint.origin.rpy: the rotation from the frame of the previous joint to this one, 
        in ROLL-PITCH-YAW XYZ convention
@@ -107,20 +113,20 @@ class ForwardKinematics(object):
      * joint.axis: (only if type is 'revolute') the axis of rotation of the joint
 
      - joint_values contains information about the current joint values in the robot. It contains
-     information about *all* the joints, and the ordering can vary, so we must find the relevant value 
-     for a particular joint you are considering. We can use the following fields:
-      * joint_values.name: a list of the names of *all* the joints in the robot
-      * joint_values.position: a list of the current values of *all* the joints in the robot, in the same 
-        order as the names in the list above.
-     To find the value of the joint we care about, we must find its name in the "name" list, then take
-     the value found at the same index in the "position" list.
+     information about ALL the joints, and the ordering can vary, so we must find the relevant
+     value for a particular joint you are considering. We can use the following fields:
+      * joint_values.name: a list of the names of ALL the joints in the robot
+      * joint_values.position: a list of the current values of ALL the joints in the robot, in
+      the same order as the names in the list above.
+     To find the value of the joint we care about, we must find its name in the "name" list, then
+     take the value found at the same index in the "position" list.
 
-    The function must return one tf message. The "transforms" field of this message must list *all* the 
-    transforms from the world coordinate frame to the links of the robot. In other words, when you are done,
-    all_transforms.transforms must contain a list in which you must place all the transforms from the 
-    "world_link" coordinate frame to each of the coordinate frames listed in "link_names". You can use the
-    "convert_to_message" function (defined above) for a convenient way to create a tf message from a 
-    transformation matrix.
+    The function must return one tf message. The "transforms" field of this message must list 
+    ALL the transforms from the world coordinate frame to the links of the robot. In other words,
+    when you are done, all_transforms.transforms must contain a list in which you must place all
+    the transforms from the "world_link" coordinate frame to each of the coordinate frames listed
+    in "link_names". You can use the "convert_to_message" function (defined above) for a convenient
+    way to create a tf message from a transformation matrix.
     """    
     def compute_transforms(self, link_names, joints, joint_values):
         all_transforms = tf.msg.tfMessage()
